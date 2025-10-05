db8b3b924fb14fc1a23c54655cb77f65d8cd9010
Oscar Karlsson <112572208+DespaZito@users.noreply.github.com>
2025-10-05 15:46:54 +0200

    Update xdc_setup.sh

--- xdc_setup.sh
+++ xdc_setup.sh
@@ -2,6 +2,6 @@
 
 # Install Dependencies
 apt update
-apt install -y ansible nano man-db python3.13-venv zip net-tools rsync
+apt install -y ansible nano man-db python3.13-venv zip net-tools rsync unminimize
 yes | unminimize
 pip3 uninstall Jinja2
